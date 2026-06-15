1c3658fc6f06eb79cf0f2bd197f22eee2254634b
Dorsa Pazooki <d.pazooki@student.fontys.nl>
2026-06-15 14:16:24 +0200

    2

--- DAL/obj/Debug/net10.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net10.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eeb270751e6dea101214be1753c4cbc761496c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e4718767cf1589cea618d94706add55c5194b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
